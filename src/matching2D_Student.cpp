f32fa61c2de5a6f07df4644b98561febe7556a9f
David Sosa <david.sosa@gmobis.com>
2020-02-06 13:25:37 +0100

    Added SIFT detector. Removed repeated function from header file

--- src/matching2D_Student.cpp
+++ src/matching2D_Student.cpp
@@ -48,10 +48,15 @@ void descKeypoints(vector<cv::KeyPoint> &keypoints, cv::Mat &img, cv::Mat &descr
 
         extractor = cv::BRISK::create(threshold, octaves, patternScale);
     }
-    else
+    else if (descriptorType == "SIFT")
     {
-
-        //...
+        int nfeatures = 0; // The number of best features to retain.
+        int nOctaveLayers = 3; // The number of layers in each octave. 3 is the value used in D. Lowe paper.
+        // The contrast threshold used to filter out weak features in semi-uniform (low-contrast) regions.
+        double contrastThreshold = 0.04;
+        double edgeThreshold = 10; // The threshold used to filter out edge-like features.
+        double sigma = 1.6; // The sigma of the Gaussian applied to the input image at the octave \#0.
+        extractor = cv::xfeatures2d::SIFT::create(nfeatures, nOctaveLayers, contrastThreshold, edgeThreshold, sigma);
     }
 
     // perform feature description
